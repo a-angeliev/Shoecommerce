95a93d5c6fde9cc87020966231cf86cddf886499
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-30 20:54:15 +0300

    Fix navbar component links

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -95,8 +95,8 @@ export const Navbar = () => {
                 <input type="email" placeholder="Your Email" />
                 <input type="password" name="" id="" placeholder="Your Password" />
                 <input type="submit" value="Login" className="login-btn" />
-                <p>Forget Password <a href="#">Reset Now</a></p>
-                <p>Don't have an account? <a href="#">Sign Up</a></p>
+                <p>Forget Password <a href="/#">Reset Now</a></p>
+                <p>Don't have an account? <a href="/#">Sign Up</a></p>
             </div>
 
         </header>
