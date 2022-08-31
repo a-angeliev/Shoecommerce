6d8684ac3e59a27acd166d07952dfd4a53aaedc6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-31 10:53:40 +0300

    Create more photos section component

---
+++ client/src/components/Products/Details/MorePhotos/MorePhotos.js
@@ -0,0 +1,34 @@
+import './MorePhotos.css';
+
+export const MorePhotos = () => {
+    return (
+        <section className="main-shoe-more--images scn">
+            <ul className="main-shoe-images-list">
+                <li className="main-shoe-img-item">
+                    <img src="https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,q_80,w_440/b3862d6f-d51b-4a56-b9e9-b3d10f37acce/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
+                        alt="Air Jordan XXXVI FS" />
+                </li>
+                <li className="main-shoe-img-item">
+                    <img src="https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,q_80,w_440/73a3c686-bcfc-4d35-8c07-f3fef164705d/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
+                        alt="Air Jordan XXXVI FS" />
+                </li>
+                <li className="main-shoe-img-item">
+                    <img src="https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,q_80,w_440/bdb9a600-174d-428e-a981-f6b7233d7cd1/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
+                        alt="Air Jordan XXXVI FS" />
+                </li>
+                <li className="main-shoe-img-item">
+                    <img src="https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,q_80,w_440/572ea72c-66d3-4be4-a940-f161fda01e1a/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
+                        alt="Air Jordan XXXVI FS" />
+                </li>
+                <li className="main-shoe-img-item">
+                    <img src="https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,q_80,w_440/9ee28f6b-be55-400e-af9c-d6419676be5a/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
+                        alt="Air Jordan XXXVI FS" />
+                </li>
+                <li className="main-shoe-img-item">
+                    <img src="https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,q_80,w_440/a427af8a-b5fa-43c2-964c-743aae124d9c/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
+                        alt="Air Jordan XXXVI FS" />
+                </li>
+            </ul>
+        </section>
+    );
+}
