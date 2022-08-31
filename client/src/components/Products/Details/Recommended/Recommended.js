d0f494ae843b87176592e19b08cc8c04b3de9664
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-31 10:46:15 +0300

    Create recommended component

---
+++ client/src/components/Products/Details/Recommended/Recommended.js
@@ -0,0 +1,89 @@
+const Recommended = () => {
+    return (
+        <section className="main-shoes-recommended scn">
+            <h2 className="main-recommended-title">
+                You may also like
+            </h2>
+            <ul className="main-recommended-list">
+                <li className="main-recommended-item">
+                    <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/f999338b-b2e7-40bc-8737-1b3a93dc5706/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
+                        alt="Air Jordan XXXVI FS" />
+                    <h4 className="main-shoe-name">
+                        Air Jordan XXXVI FS
+                    </h4>
+                    <h5 className="main-shoe-price">
+                        $369.99
+                    </h5>
+                    <button className="main-shoe-button">
+                        Find More
+                    </button>
+                </li>
+                <li className="main-recommended-item">
+                    <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/f999338b-b2e7-40bc-8737-1b3a93dc5706/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
+                        alt="Air Jordan XXXVI FS" />
+                    <h4 className="main-shoe-name">
+                        Air Jordan XXXVI FS
+                    </h4>
+                    <h5 className="main-shoe-price">
+                        $369.99
+                    </h5>
+                    <button className="main-shoe-button">
+                        Find More
+                    </button>
+                </li>
+                <li className="main-recommended-item">
+                    <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/f999338b-b2e7-40bc-8737-1b3a93dc5706/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
+                        alt="Air Jordan XXXVI FS" />
+                    <h4 className="main-shoe-name">
+                        Air Jordan XXXVI FS
+                    </h4>
+                    <h5 className="main-shoe-price">
+                        $369.99
+                    </h5>
+                    <button className="main-shoe-button">
+                        Find More
+                    </button>
+                </li>
+                <li className="main-recommended-item">
+                    <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/f999338b-b2e7-40bc-8737-1b3a93dc5706/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
+                        alt="Air Jordan XXXVI FS" />
+                    <h4 className="main-shoe-name">
+                        Air Jordan XXXVI FS
+                    </h4>
+                    <h5 className="main-shoe-price">
+                        $369.99
+                    </h5>
+                    <button className="main-shoe-button">
+                        Find More
+                    </button>
+                </li>
+                <li className="main-recommended-item">
+                    <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/f999338b-b2e7-40bc-8737-1b3a93dc5706/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
+                        alt="Air Jordan XXXVI FS" />
+                    <h4 className="main-shoe-name">
+                        Air Jordan XXXVI FS
+                    </h4>
+                    <h5 className="main-shoe-price">
+                        $369.99
+                    </h5>
+                    <button className="main-shoe-button">
+                        Find More
+                    </button>
+                </li>
+                <li className="main-recommended-item">
+                    <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/f999338b-b2e7-40bc-8737-1b3a93dc5706/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
+                        alt="Air Jordan XXXVI FS" />
+                    <h4 className="main-shoe-name">
+                        Air Jordan XXXVI FS
+                    </h4>
+                    <h5 className="main-shoe-price">
+                        $369.99
+                    </h5>
+                    <button className="main-shoe-button">
+                        Find More
+                    </button>
+                </li>
+            </ul>
+        </section>
+    );
+}
