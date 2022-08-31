81081b348c25a5c887bccfffb730aeb8270d4693
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2022-08-31 08:41:23 +0300

    move components to home page

--- client/src/App.js
+++ client/src/App.js
@@ -2,53 +2,24 @@ import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
-import { Navbar } from "./components/Navbar/Navbar";
-import { Home } from "./components/Home/Home";
-import { NewArrival } from "./components/New Arrival/NewArrival";
-import { Products } from "./components/Products/Products";
-import { Details } from "./components/Products/Details/Details";
-import { Reviews } from "./components/Reviews/Reviews";
-import { Footer } from "./components/Footer/Footer";
-import { Copyright } from "./components/Copyright/Copyright";
+import { Navbar } from './components/Navbar/Navbar';
+import { Home } from './components/Home/Home';
+import { Details } from './components/Products/Details/Details';
+import { Footer } from './components/Footer/Footer';
+import { Copyright } from './components/Copyright/Copyright';
 
 function App() {
-  return (
-    <div className="App">
-
-      {/* Navbar */}
-
-      <Navbar />
-
-      {/* <!-- Home --> */}
-      {/* <Home /> */}
-
-      {/* <!-- New Arrival --> */}
-
-      {/* <NewArrival /> */}
-
-      {/* <!-- Products --> */}
-
-      {/* <Products /> */}
-
-      {/* <!-- Products --> */}
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/products/:id' element={<Details />} />
-      </Routes>
-
-
-      {/* <Reviews /> */}
-
-      {/* <!-- Footer --> */}
-
-      {/* <Footer /> */}
-
-      {/* <!-- Copyright --> */}
-
-      {/* <Copyright /> */}
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/products/:id" element={<Details />} />
+            </Routes>
+            <Footer />
+            <Copyright />
+        </div>
+    );
 }
 
 export default App;
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,18 +1,29 @@
-import "./Home.css"
+import { NewArrival } from '../New Arrival/NewArrival';
+import { Products } from '../Products/Products';
+import { Reviews } from '../Reviews/Reviews';
+import './Home.css';
 
 export const Home = () => {
-
-
     return (
-        <section className="home" id="home">
-            <div className="home-text">
-                <span>Shop Now</span>
-                <h1>New Arrival of <br /> Fresh Products</h1>
-                <a href="/Shop" className="btn">Shop Now</a>
-            </div>
-            <div className="home-img">
-                <img src="/images/image-1600-14.jpg" alt="" />
-            </div>
-        </section>
-    )
-}
+        <>
+            <section className="home" id="home">
+                <div className="home-text">
+                    <span>Shop Now</span>
+                    <h1>
+                        New Arrival of <br /> Fresh Products
+                    </h1>
+                    <a href="/Shop" className="btn">
+                        Shop Now
+                    </a>
+                </div>
+                <div className="home-img">
+                    <img src="/images/image-1600-14.jpg" alt="" />
+                </div>
+            </section>
+
+            <NewArrival />
+            <Products />
+            <Reviews />
+        </>
+    );
+};
