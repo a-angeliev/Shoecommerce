8800576fadfd1b3ed1007272569c68743a49553d
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2022-09-03 01:55:22 +0300

    implement image upload func

---
+++ server/services/cloudinary.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from flask import jsonify
+
+load_dotenv()
+
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+config = cloudinary.config(secure=True)
+
+
+class Cloudinary:
+
+    @staticmethod
+    def upload_img(uploaded_file):
+        upload_result = cloudinary.uploader.upload(uploaded_file)
+        return jsonify(upload_result)
