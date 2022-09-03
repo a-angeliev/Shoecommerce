7af00e4da79cd890d6b1cd5d43503c19b74adee8
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2022-09-03 16:58:38 +0300

    fix resources validation

--- server/resources/products.py
+++ server/resources/products.py
@@ -2,16 +2,18 @@ from flask import request
 from flask_restful import Resource
 
 from managers.products import ProductManager
+from models import RoleType
 from schemas.request.product import CreateProductRequestSchema
 from schemas.response.product import CreateProductResponseSchema
-from utils.decorators import validate_schema
+from utils.decorators import validate_schema, permission_required, token_required
 
 
 class Products(Resource):
     @staticmethod
+    @token_required
+    @permission_required(RoleType.admin)
     @validate_schema(CreateProductRequestSchema)
     def post():
-        # ProductManager.create_product()
         product = ProductManager.create_product(request.get_json())
         schema = CreateProductResponseSchema()
         return schema.dumps(product)
