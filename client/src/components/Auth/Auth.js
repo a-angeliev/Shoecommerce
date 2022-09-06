e747ddf4bf203c036b80092963fa520e2e121aed
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-06 22:28:25 +0300

    Fix auth component sign in and sign up logic

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,12 +1,34 @@
-export const Auth = () => {
+import { useState } from "react";
+import { Link } from 'react-router-dom'
+
+export const Auth = ({ activeIcon }) => {
+    const [isSignIn, setIsSignIn] = useState(true);
+
+    const handleSwitchMode = () => {
+        setIsSignIn(prevState => !prevState);
+    }
+
     return (
-        <div className="user" ref={userRef}>
-            <h2>Login Now</h2>
+        <div className={`user ${activeIcon === 'user' && 'active'}`}>
+            <h2>
+                {isSignIn ? 'Login Now' : 'Register Now'}
+            </h2>
             <input type="email" placeholder="Your Email" />
             <input type="password" name="" id="" placeholder="Your Password" />
+            {!isSignIn &&
+                <input type="password" name="" id="" placeholder="Repeat Password" />
+            }
             <input type="submit" value="Login" className="login-btn" />
             <p>Forget Password <a href="/#">Reset Now</a></p>
-            <p>Don't have an account? <a href="/#">Sign Up</a></p>
+            <p className="user-auth-question">
+                {isSignIn ? 'Don\'t have an account?' : 'Already have an account?'}
+                <Link
+                    to={''}
+                    onClick={handleSwitchMode}
+                >
+                    {isSignIn ? 'Sign Up' : 'Sign In'}
+                </Link>
+            </p>
         </div>
     );
 }
