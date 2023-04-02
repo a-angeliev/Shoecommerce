dfdef8275d43611ca15cbbc8052f8395ebcddf5c
a-angeliev <nachko01@gmail.com>
2023-04-02 15:39:08 +0300

    add category filter, and sync to work with brand fiter

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -9,46 +9,77 @@ export const Products = () => {
     const { products } = useContext(ProductContext);
     const [filteredProductsByGender, setFilteredProductsByGender] = useState([]);
     const [availableBrands, setAvailableBrands] = useState([]);
-	const [brandFilter, setBrandFilter] = useState([])
-	const [filteredProductsForDisplay, setFilteredProductsForDisplay] = useState([])
-
+    const [availableCategories, setAvailableCategories] = useState([]);
+    const [brandFilter, setBrandFilter] = useState([]);
+    const [categoryFilter, setCategoryFilter] = useState([]);
+    const [filteredProductsForDisplay, setFilteredProductsForDisplay] = useState([]);
+    const [isFilter, setIsFilter] = useState(false);
     const navigate = useNavigate();
-
 
     useEffect(() => {
         if (products) {
-            const filteredProducts = products.filter(
-                (product) => product.gender === gender
-            );
-            const availableBrands = [...new Set(filteredProducts.map(product => product.brand.name))]
+            const filteredProducts = products.filter((product) => product.gender === gender);
+            const availableBrands = [...new Set(filteredProducts.map((product) => product.brand.name))];
+            const availableCategories = [...new Set(filteredProducts.map((product) => product.category.title))];
             setFilteredProductsByGender(filteredProducts);
             setAvailableBrands(availableBrands);
+            setAvailableCategories(availableCategories);
         }
-		setBrandFilter([])
+        setBrandFilter([]);
+        setCategoryFilter([]);
     }, [gender]);
 
-	useEffect(() => {
-		const products = [...filteredProductsByGender]
-		const filteredProductsByBrands = products.filter(product => brandFilter.includes(product.brand.name)? product : null)
-		setFilteredProductsForDisplay(filteredProductsByBrands)
-		console.log(filteredProductsByBrands,123)
-	}, [brandFilter])
+
+
+    useEffect(() => {
+        const products = [...filteredProductsByGender];
+        const filteredProductsByBrands = products.filter((product) => filterProductByCategoryAndBrand(product));
+        setFilteredProductsForDisplay(filteredProductsByBrands);
+        setIsFilter(brandFilter.length == 0 && categoryFilter.length == 0 ? false : true);
+    }, [brandFilter, categoryFilter]);
 
     const goToShoeDetailPage = (id) => {
         let path = "/product/" + id;
         navigate(path);
     };
 
-	const handleBrandChanges = (brand) => {
-		if(!brandFilter.includes(brand)){
-			setBrandFilter([...brandFilter, brand])
-		}else{
-			const brands = [...brandFilter]
-			brands.splice(brands.indexOf(brand),1)
-			setBrandFilter(brands)
-		}
-	}
-	
+    const filterProductByCategoryAndBrand = (product) => {
+        let flag = true;
+        if (brandFilter.length == 0 && categoryFilter.length == 0) {
+            return product;
+        }
+
+        if (brandFilter.length != 0) {
+            flag = brandFilter.includes(product.brand.name);
+        }
+        if (categoryFilter.length != 0 && flag) {
+            flag = categoryFilter.includes(product.category.title);
+        }
+        if (flag) {
+            return product;
+        }
+    };
+    
+    const handleBrandChanges = (brand) => {
+        if (!brandFilter.includes(brand)) {
+            setBrandFilter([...brandFilter, brand]);
+        } else {
+            const brands = [...brandFilter];
+            brands.splice(brands.indexOf(brand), 1);
+            setBrandFilter(brands);
+        }
+    };
+
+    const handleCategoryChanges = (category) => {
+        if (!categoryFilter.includes(category)) {
+            setCategoryFilter([...categoryFilter, category]);
+        } else {
+            const categories = [...categoryFilter];
+            categories.splice(categories.indexOf(category), 1);
+            setCategoryFilter(categories);
+        }
+    };
+
     const displayProduct = (product) => {
         return (
             <div className='catalog-items-container'>
@@ -59,9 +90,7 @@ export const Products = () => {
                     <h1>{product.title}</h1>
                     <div className='priceB'>
                         <span>{product.price} USD</span>
-                        <i
-                            onClick={() => goToShoeDetailPage(product.id)}
-                            className='bx bx-cart-alt'></i>
+                        <i onClick={() => goToShoeDetailPage(product.id)} className='bx bx-cart-alt'></i>
                     </div>
                 </div>
             </div>
@@ -80,17 +109,37 @@ export const Products = () => {
                         {/* <li><Link to="">Male</Link></li>
 							<li><Link to="">Female</Link></li>
 							<li><Link to="">Children</Link></li> */}
-						{availableBrands ? <h3 className = "brand-title">Brands</h3> : null}
+                        {availableBrands ? <h3 className='brand-title'>Brands</h3> : null}
                         {availableBrands.map((brand, idx) => (
-							<div>
-								<label htmlFor={brand}>{brand}</label>
-								<input checked={brandFilter.includes(brand) ? true : false} onChange = {()=> handleBrandChanges(brand)} type="checkbox" name={brand} key={idx}></input>
-							</div>
+                            <div>
+                                <label htmlFor={brand}>{brand}</label>
+                                <input
+                                    checked={brandFilter.includes(brand) ? true : false}
+                                    onChange={() => handleBrandChanges(brand)}
+                                    type='checkbox'
+                                    name={brand}
+                                    key={idx}></input>
+                            </div>
+                        ))}
+
+                        {availableCategories ? <h3 className='category-title'>Categories</h3> : null}
+                        {availableCategories.map((category, idx) => (
+                            <div>
+                                <label htmlFor={category}>{category}</label>
+                                <input
+                                    checked={categoryFilter.includes(category) ? true : false}
+                                    onChange={() => handleCategoryChanges(category)}
+                                    type='checkbox'
+                                    name={category}
+                                    key={idx}></input>
+                            </div>
                         ))}
                     </ul>
                 </div>
             </div>
-            {filteredProductsForDisplay.length != 0? filteredProductsForDisplay.map((product) => displayProduct(product)): filteredProductsByGender.map((product) => displayProduct(product))}
+            {isFilter
+                ? filteredProductsForDisplay.map((product) => displayProduct(product))
+                : filteredProductsByGender.map((product) => displayProduct(product))}
             <div className='catalog-items-container'>
                 <div className='box'>
                     <img
