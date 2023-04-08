9a68199242d18a7de3658c96b218116d170397ca
a-angeliev <nachko01@gmail.com>
2023-04-08 17:25:16 +0300

    add real recommender shoes in details page

--- client/src/components/Products/Details/Recommended/Recommended.js
+++ client/src/components/Products/Details/Recommended/Recommended.js
@@ -1,94 +1,52 @@
-import './Recommended.css';
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ProductContext } from "../../../../contexts/productContext";
+import "./Recommended.css";
 
 export const Recommended = (props) => {
-    const gender = props
-    console.log(gender.gender)
+    const gender = props.gender;
+    const [recommendedProducts, setRecommenderProducts] = useState([]);
+    const [path, setPath] = useState("");
+    const { products } = useContext(ProductContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (typeof products !== "string") {
+            console.log(gender);
+            const filteredProducts = products.filter((product) => product.gender === gender);
+            filteredProducts.sort(() => Math.random() - 0.5);
+            setRecommenderProducts(filteredProducts.slice(0, 6));
+        }
+    }, [products, path]);
+
+    const goToShoeDetailPage = (id) => {
+        window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+        let path = "/product/" + id;
+        setPath(path);
+        navigate(path);
+    };
+
+    const listRecommendedProduct = (product) => {
+        return (
+            <li className='main-recommended-item'>
+                <img src={product.images[0].img_url} alt='shoe picture' />
+                <h4 className='main-shoe-name'>{product.title}</h4>
+                <h5 className='main-shoe-price'>{product.price} USD</h5>
+                <button onClick={() => goToShoeDetailPage(product.id)} className='main-shoe-button'>
+                    Find More
+                </button>
+            </li>
+        );
+    };
 
     return (
-        <section className="main-shoes-recommended scn">
-            <h2 className="main-recommended-title">
-                You may also like
-            </h2>
-            <ul className="main-recommended-list">
-                <li className="main-recommended-item">
-                    <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/f999338b-b2e7-40bc-8737-1b3a93dc5706/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
-                        alt="Air Jordan XXXVI FS" />
-                    <h4 className="main-shoe-name">
-                        Air Jordan XXXVI FS
-                    </h4>
-                    <h5 className="main-shoe-price">
-                        $369.99
-                    </h5>
-                    <button className="main-shoe-button">
-                        Find More
-                    </button>
-                </li>
-                <li className="main-recommended-item">
-                    <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/f999338b-b2e7-40bc-8737-1b3a93dc5706/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
-                        alt="Air Jordan XXXVI FS" />
-                    <h4 className="main-shoe-name">
-                        Air Jordan XXXVI FS
-                    </h4>
-                    <h5 className="main-shoe-price">
-                        $369.99
-                    </h5>
-                    <button className="main-shoe-button">
-                        Find More
-                    </button>
-                </li>
-                <li className="main-recommended-item">
-                    <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/f999338b-b2e7-40bc-8737-1b3a93dc5706/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
-                        alt="Air Jordan XXXVI FS" />
-                    <h4 className="main-shoe-name">
-                        Air Jordan XXXVI FS
-                    </h4>
-                    <h5 className="main-shoe-price">
-                        $369.99
-                    </h5>
-                    <button className="main-shoe-button">
-                        Find More
-                    </button>
-                </li>
-                <li className="main-recommended-item">
-                    <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/f999338b-b2e7-40bc-8737-1b3a93dc5706/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
-                        alt="Air Jordan XXXVI FS" />
-                    <h4 className="main-shoe-name">
-                        Air Jordan XXXVI FS
-                    </h4>
-                    <h5 className="main-shoe-price">
-                        $369.99
-                    </h5>
-                    <button className="main-shoe-button">
-                        Find More
-                    </button>
-                </li>
-                <li className="main-recommended-item">
-                    <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/f999338b-b2e7-40bc-8737-1b3a93dc5706/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
-                        alt="Air Jordan XXXVI FS" />
-                    <h4 className="main-shoe-name">
-                        Air Jordan XXXVI FS
-                    </h4>
-                    <h5 className="main-shoe-price">
-                        $369.99
-                    </h5>
-                    <button className="main-shoe-button">
-                        Find More
-                    </button>
-                </li>
-                <li className="main-recommended-item">
-                    <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/f999338b-b2e7-40bc-8737-1b3a93dc5706/air-jordan-xxxvi-fs-basketball-shoes-BdpjNf.png"
-                        alt="Air Jordan XXXVI FS" />
-                    <h4 className="main-shoe-name">
-                        Air Jordan XXXVI FS
-                    </h4>
-                    <h5 className="main-shoe-price">
-                        $369.99
-                    </h5>
-                    <button className="main-shoe-button">
-                        Find More
-                    </button>
-                </li>
+        <section className='main-shoes-recommended scn'>
+            <h2 className='main-recommended-title'>You may also like</h2>
+            <ul className='main-recommended-list'>
+                {recommendedProducts.length !== 0
+                    ? recommendedProducts.map((product) => listRecommendedProduct(product))
+                    : null}
             </ul>
         </section>
     );
-}
+};
