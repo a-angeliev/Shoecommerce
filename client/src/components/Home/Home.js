932a3a03d6594de1b3d6bc274bd4067599b873f7
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2023-04-09 21:41:59 +0300

    fix shoe product card margins

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,26 +1,25 @@
-import { useContext } from 'react';
-import { ProductContext } from '../../contexts/productContext';
-import { NewArrival } from '../New Arrival/NewArrival';
-import { Products } from '../Products/Products';
-import { Reviews } from '../Reviews/Reviews';
-import './Home.css';
+import { useContext } from "react";
+import { ProductContext } from "../../contexts/productContext";
+import { NewArrival } from "../New Arrival/NewArrival";
+import { Products } from "../Products/Products";
+import { Reviews } from "../Reviews/Reviews";
+import "./Home.css";
 
 export const Home = () => {
-    
     return (
         <>
-            <section className="home" id="home">
-                <div className="home-text">
+            <section className='home' id='home'>
+                <div className='home-text'>
                     <span>Shop Now</span>
                     <h1>
                         New Arrival of <br /> Fresh Products
                     </h1>
-                    <a href="/Shop" className="btn">
+                    <a href='#new' className='btn'>
                         Shop Now
                     </a>
                 </div>
-                <div className="home-img">
-                    <img src="/images/image-1600-14.jpg" alt="" />
+                <div className='home-img'>
+                    <img src='/images/image-1600-14.jpg' alt='' />
                 </div>
             </section>
 
