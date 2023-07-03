e56aa5e8849ac7673e6869df99a91bb358bf11f7
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2023-07-03 19:54:27 +0300

    implement full brand create functionality

--- client-admin/src/components/Brand/BrandForm/BrandForm.js
+++ client-admin/src/components/Brand/BrandForm/BrandForm.js
@@ -1,7 +1,70 @@
 import style from "./BrandForm.module.css";
 
+import { useState } from "react";
+
+import * as brandService from "../../../services/brand";
+import { useNavigate } from "react-router-dom";
+
 export const BrandForm = (params) => {
-    console.log(params);
+    const [brandInput, setBrandInput] = useState({ name: "", "logo-url": "", description: "" });
+    const [nameValidation, setNameValidation] = useState("");
+    const [logoValidation, setLogoValidation] = useState("");
+    const [descValidation, setDescValidation] = useState("");
+
+    const navigate = useNavigate();
+
+    const inputHandler = (e) => {
+        const data = { ...brandInput };
+        data[e.target.name] = e.target.value;
+        setBrandInput(data);
+        validate();
+    };
+
+    const validate = () => {
+        console.log(brandInput);
+        if (brandInput["name"].length <= 2) {
+            setNameValidation(false);
+        } else {
+            setNameValidation(true);
+        }
+        if (brandInput["logo-url"].length < 5) {
+            setLogoValidation(false);
+        } else {
+            setLogoValidation(true);
+        }
+        if (brandInput["description"].length <= 2) {
+            setDescValidation(false);
+        } else {
+            setDescValidation(true);
+        }
+    };
+
+    const checkValidation = () => {
+        if (nameValidation && logoValidation && descValidation) {
+            return true;
+        }
+        return false;
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (checkValidation()) {
+            const brandData = {
+                name: brandInput.name,
+                logo_url: brandInput["logo-url"],
+                description: brandInput.description,
+            };
+
+            const brand = brandService
+                .createBrand(brandData)
+                .then((res) => {
+                    console.log(res);
+                    navigate("/brand/information");
+                })
+                .then((err) => console.log(err));
+        }
+    };
+
     return (
         <>
             <div className={style["page-content"]}>
@@ -11,18 +74,35 @@ export const BrandForm = (params) => {
                         <div className={style.title}>
                             <h2>Brand Form</h2>
                         </div>
-                        <div className={style.form}>
-                            <label htmlFor='name'>Name</label>
-                            <input name='name' type='text'></input>
-
-                            <label htmlFor='logo-url'>Logo URL</label>
-                            <input name='logo-url' type='url'></input>
-
-                            <label htmlFor='description'>Description</label>
-                            <textarea name='description' className={style.description} type='textarea'></textarea>
-
-                            <button name='button'>Submit</button>
-                        </div>
+                        <form onSubmit={onSubmit}>
+                            <div className={style.form}>
+                                <label htmlFor='name'>Name</label>
+                                <input
+                                    className={nameValidation === true ? "" : style.error}
+                                    name='name'
+                                    id='name'
+                                    type='text'
+                                    onChange={(e) => inputHandler(e)}
+                                    value={brandInput.name}></input>
+                                <label htmlFor='logo-url'>Logo URL</label>
+                                <input
+                                    className={logoValidation === true ? "" : style.error}
+                                    name='logo-url'
+                                    id='logo-url'
+                                    type='url'
+                                    onChange={(e) => inputHandler(e)}
+                                    value={brandInput["logo-url"]}></input>
+                                <label htmlFor='description'>Description</label>
+                                <textarea
+                                    name='description'
+                                    id='description'
+                                    className={`${style.description}  ${descValidation === true ? "" : style.error} `}
+                                    onChange={(e) => inputHandler(e)}
+                                    value={brandInput.description}
+                                    type='textarea'></textarea>
+                                <button name='button'>Submit</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
