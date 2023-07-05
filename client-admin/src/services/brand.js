388a4f59d16e634228263ca51754cec2c9e0179d
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2023-07-05 13:10:27 +0300

    refactor services ednpoints

--- client-admin/src/services/brand.js
+++ client-admin/src/services/brand.js
@@ -1,29 +1,29 @@
 import * as request from "./request.js";
 
 const endpoints = {
-    ALLBRANDS: "/brand",
-    ADDBRAND: "/brand",
-    BRANDBYID: (brandId) => `/brand/${brandId}`,
-    EDITBRAND: (brandId) => `/brand/${brandId}`,
-    DELETEBRAND: (brandId) => `/brand/${brandId}`,
+    ALL_BRANDS: "/brand",
+    ADD_BRAND: "/brand",
+    BRAND_BY_ID: (brandId) => `/brand/${brandId}`,
+    EDIT_BRAND: (brandId) => `/brand/${brandId}`,
+    DELETE_BRAND: (brandId) => `/brand/${brandId}`,
 };
 
 export async function getAllBrands() {
-    return request.get(endpoints.ALLBRANDS);
+    return request.get(endpoints.ALL_BRANDS);
 }
 
 export async function createBrand(brandData) {
-    return request.post(endpoints.ADDBRAND, brandData);
+    return request.post(endpoints.ADD_BRAND, brandData);
 }
 
 export async function getBrandById(brandId) {
-    return request.get(endpoints.BRANDBYID(brandId));
+    return request.get(endpoints.BRAND_BY_ID(brandId));
 }
 
 export async function editBrandById(brandId, brandData) {
-    return request.put(endpoints.EDITBRAND(brandId), brandData);
+    return request.put(endpoints.EDIT_BRAND(brandId), brandData);
 }
 
 export async function deleteBrandById(brandId) {
-    return request.del(endpoints.DELETEBRAND(brandId));
+    return request.del(endpoints.DELETE_BRAND(brandId));
 }
--- client-admin/src/services/category.js
+++ client-admin/src/services/category.js
@@ -1,29 +1,29 @@
 import * as request from "./request.js";
 
 const endpoints = {
-    GETALLCATEGORIES: "/category",
-    CREATECATEGORY: "/category",
-    GETCATEGORYBYID: (categoryId) => `/category/${categoryId}`,
-    EDITCATEGORY: (categoryId) => `/category/${categoryId}`,
-    DELETECATEGORY: (categoryId) => `/category/${categoryId}`,
+    GET_ALL_CATEGORIES: "/category",
+    CREATE_CATEGORY: "/category",
+    GET_CATEGORY_BY_ID: (categoryId) => `/category/${categoryId}`,
+    EDIT_CATEGORY: (categoryId) => `/category/${categoryId}`,
+    DELETE_CATEGORY: (categoryId) => `/category/${categoryId}`,
 };
 
 export async function getAllCategories() {
-    return request.get(endpoints.GETALLCATEGORIES);
+    return request.get(endpoints.GET_ALL_CATEGORIES);
 }
 
 export async function createCategory(categoryData) {
-    return request.post(endpoints.CREATECATEGORY, categoryData);
+    return request.post(endpoints.CREATE_CATEGORY, categoryData);
 }
 
 export async function getCategoryById(categoryId) {
-    return request.get(endpoints.GETCATEGORYBYID(categoryId));
+    return request.get(endpoints.GET_CATEGORY_BY_ID(categoryId));
 }
 
 export async function editCategory(categoryId, categoryData) {
-    return request.put(endpoints.EDITCATEGORY(categoryId), categoryData);
+    return request.put(endpoints.EDIT_CATEGORY(categoryId), categoryData);
 }
 
 export async function deleteCategory(categoryId) {
-    return request.del(endpoints.DELETECATEGORY(categoryId));
+    return request.del(endpoints.DELETE_CATEGORY(categoryId));
 }
