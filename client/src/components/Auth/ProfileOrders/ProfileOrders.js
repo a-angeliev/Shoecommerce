20a39dd9d93ec3d3d8c1170958e14e756b227a74
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2023-08-04 00:08:37 +0300

    refactor the profileOrder component

--- client/src/components/Auth/ProfileOrders/ProfileOrders.js
+++ client/src/components/Auth/ProfileOrders/ProfileOrders.js
@@ -1,24 +1,27 @@
-import "./ProfileOrders.css";
-import { OrderInfo } from "./OrderInfo/OrderInfo";
 import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "../../../contexts/Auth";
 
+import { AuthContext } from "../../../contexts/Auth";
+import { AlertContext } from "../../../contexts/alertContext";
+import { OrderInfo } from "./OrderInfo/OrderInfo";
 import * as userRequest from "../../../services/user";
+
+import "./ProfileOrders.css";
 
 export const ProfileOrders = () => {
     const { user } = useContext(AuthContext);
+    const { setAlert } = useContext(AlertContext);
+
     const [userOrders, setUserOrders] = useState([]);
 
     useEffect(() => {
-        takeUserOrders();
-    }, []);
-
-    const takeUserOrders = () => {
         userRequest
             .getOrders(user["user_id"])
-            .then((res) => setUserOrders(JSON.parse(res)))
-            .catch((err) => console.log(err));
-    };
+            .then((user) => setUserOrders(JSON.parse(user)))
+            .catch((err) => {
+                console.log(err);
+                setAlert({ color: "red", text: "Somethings gone wrong. Try to reload the page!" });
+            });
+    }, []);
 
     return (
         <>
