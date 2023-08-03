b956d1650cf110fb347f74bd161e5cb9839ba414
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2023-08-03 23:07:08 +0300

    refactor the profileMenu component / client

--- client/src/components/Auth/ProfileMenu/ProfileMenu.js
+++ client/src/components/Auth/ProfileMenu/ProfileMenu.js
@@ -1,35 +1,30 @@
 import { useContext } from "react";
-import "./ProfileMenu.css";
+
 import { AuthContext } from "../../../contexts/Auth";
-import { useNavigate } from "react-router-dom";
-import { ActiveIconContext } from "../../../contexts/activeIconContext";
 import { useNav } from "../../../hooks/useNavigation";
+
+import "./ProfileMenu.css";
 
 export const ProfileMenu = () => {
     const { userLogout } = useContext(AuthContext);
-    const { setActiveIcon } = useContext(ActiveIconContext);
-    const navigate = useNavigate();
     const navTo = useNav();
 
     const logout = () => {
         userLogout();
         navTo("/");
     };
+
     return (
         <div className='profile-menu'>
             <ul className='profile-menu-list'>
                 <li
                     onClick={() => {
-                        // setActiveIcon("");
-                        // navigate("/user/details");
                         navTo("/user/details");
                     }}>
                     Profile
                 </li>
                 <li
                     onClick={() => {
-                        // setActiveIcon("");
-                        // navigate("/user/orders");
                         navTo("/user/orders");
                     }}>
                     Orders
