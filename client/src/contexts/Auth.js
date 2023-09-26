b4787ab33e8663b9a91e9c5ebeabd04c4ebbff45
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2023-09-26 18:16:51 +0300

    format the Auth context component

--- client/src/contexts/Auth.js
+++ client/src/contexts/Auth.js
@@ -1,49 +1,19 @@
-// import { createContext, useContext } from 'react';
-// import useLocalStorage from '../hooks/useLocalStorage';
-
-// export const AuthContext = createContext();
-
-// export const AuthProvider = ({ children }) => {
-//     const [user, setItem, removeUser] = useLocalStorage('user', null);
-
-//     const addUser = (user) => setItem(user);
-
-//     const logout = () => {
-//         removeUser();
-//     };
-
-//     const isAuthenticated = Boolean(user?.token);
-
-//     return (
-//         <AuthContext.Provider
-//             value={{ user, addUser, logout, isAuthenticated }}
-//         >
-//             {children}
-//         </AuthContext.Provider>
-//     );
-// };
-
-// /**
-//  *
-//  * @returns { { user: {}, addUser: ({ email: string, password: string}) => void, logout: () => void, isAuthenticated: boolean} }
-//  */
-// export const useAuthContext = () => {
-//     const authState = useContext(AuthContext);
-
-//     return authState;
-// };
-
 import { createContext, useContext, useEffect } from "react";
-import { useLocalStorage } from "./../hooks/useLocalStorage";
+
 import { AlertContext } from "./alertContext";
+import { useLocalStorage } from "./../hooks/useLocalStorage";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const { setAlert } = useContext(AlertContext);
-    const [auth, setAuth] = useLocalStorage("auth", {});
-    const userLogin = (authData) => setAuth(authData);
+
     const userLogout = () => setAuth({});
+
+    const [auth, setAuth] = useLocalStorage("auth", {});
+
+    const userLogin = (authData) => setAuth(authData);
+
     const isAuthenticated = Boolean(auth?.token);
 
     useEffect(() => {
