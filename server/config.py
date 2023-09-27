49c123c4233e8039c7a07d9cab893d37e6580f02
a-angeliev <89412429+a-angeliev@users.noreply.github.com>
2023-09-27 15:20:23 +0300

    remove testing env configurations

--- server/config.py
+++ server/config.py
@@ -14,13 +14,13 @@ from schemas import request
 class DevelopmentConfig:
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:{config('DB_PORT')}/{config('DB_NAME')}"
+    # SQLALCHEMY_DATABASE_URI = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:{config('DB_PORT')}/{config('DB_NAME')}"
 
 
 class TestConfig:
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:{config('DB_PORT')}/{config('TEST_DB_NAME')}"
+    # SQLALCHEMY_DATABASE_URI = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:{config('DB_PORT')}/{config('TEST_DB_NAME')}"
 
 class ProductionConfig:
     DEBUG = False
